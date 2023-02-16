ffd1854fe8f75ba10f940a5f29411784f21fb47f
Giuseppe <85261127+GiuseppeF96@users.noreply.github.com>
2023-02-16 19:05:37 +0100

    Small format changes

--- src/controllers/seriesController.js
+++ src/controllers/seriesController.js
@@ -1,54 +1,54 @@
 import { check, validationResult } from "express-validator";
 const series = [
-    {
-        id: 1,
-        name: 'The Walking Dead',
-        genre: 'Horror',
-        seasons: 10,
-        episodes: 180,
-    },
-    {
-        id: 2,
-        name: 'Breaking Bad',
-        genre: 'Drama',
-        seasons: 5,
-        episodes: 62,
-    },
-    {
-        id: 3,
-        name: 'Game of Thrones',
-        genre: 'Fantasy',
-        seasons: 8,
-        episodes: 73,
-    },
+  {
+    id: 1,
+    name: "The Walking Dead",
+    genre: "Horror",
+    seasons: 10,
+    episodes: 180,
+  },
+  {
+    id: 2,
+    name: "Breaking Bad",
+    genre: "Drama",
+    seasons: 5,
+    episodes: 62,
+  },
+  {
+    id: 3,
+    name: "Game of Thrones",
+    genre: "Fantasy",
+    seasons: 8,
+    episodes: 73,
+  },
 ];
 
 export const getSeries = (req, res) => {
-    res.status(200).send(series);
+  res.status(200).send(series);
 };
 
 export const findSeries = (req, res) => {
-    let result = series.filter((serie) => serie.name == req.query.name);
-    res.status(200).send(result);
+  let result = series.filter((serie) => serie.name == req.query.name);
+  res.status(200).send(result);
 };
 
 export const findSeriesById = (req, res) => {
-    let result = series.filter((serie) => serie.id == req.params.id);
-    res.status(200).send(result);
+  let result = series.filter((serie) => serie.id == req.params.id);
+  res.status(200).send(result);
 };
 
 export const addSerie = (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    let serie = req.body;
-    series.push(serie);
-    res.status(201).send(`Added ${serie.title} to series collection`);
-  };
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  let serie = req.body;
+  series.push(serie);
+  res.status(201).send(`Added ${serie.name} to series collection`);
+};
 
 // attached as second param in a route
 export const newSerieValidators = [
-    check("name").notEmpty().withMessage("Name field required"),
-    check("genre").notEmpty().withMessage("Genre field required"),
-  ];
+  check("name").notEmpty().withMessage("Name field required"),
+  check("genre").notEmpty().withMessage("Genre field required"),
+];
