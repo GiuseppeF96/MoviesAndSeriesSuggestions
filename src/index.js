03060d433c8a594af9475d7b65e5d72c55c58ee2
cursedsound <cursedsoundproductions@gmail.com>
2023-02-16 18:38:42 +0100

    created crontoller and router for series

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import express from "express";
 import bodyParser from "body-parser";
+import seriesRoutes from "./routers/seriesRoutes.js";
 const app = express();
 
 const port = 4000;
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello World!");
-});
+// app uses json
+app.use(bodyParser.json());
+app.use("/series", seriesRoutes);
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
---
+++ src/routers/seriesRoutes.js
@@ -0,0 +1,17 @@
+import express from "express";
+import {
+    getSeries,
+    findSeries,
+    findSeriesById,
+    addSerie,
+    newSerieValidators,
+} from "../controllers/seriesController.js";
+
+const router = express.Router();
+
+router.get("/", getSeries);
+router.get("/search", findSeries);
+router.get("/:id", findSeriesById);
+router.post("/", newSerieValidators, addSerie);
+
+export default router;
