01d64fc1695854aab5a01f9f6158fb8aa72f03c6
ManuelBleuel <125586529+ManuelBleuel@users.noreply.github.com>
2023-02-21 19:51:03 +0100

    small Changes

--- src/controllers/classicController.js
+++ src/controllers/classicController.js
@@ -1,68 +1,66 @@
 import { check, validationResult } from "express-validator";
 const classic = [
-    {
-        id: 1,
-        name: "Psycho",
-        genre: "Horror",
-        publishing year: 1960,
-        length: 109,
-        director: "Alfred Hitchcock",
-    },
-    {
-        id: 2,
-        name: "The Good, the Bad and the Ugly",
-        genre: "Western",
-        publishing year: 1966,
-        length: 161,
-	director: "Sergio Leone",
-    },
-    {
-        id: 3,
-        name: "Balde Runner",
-        genre: "Sci-Fi",
-        publishing year: 1982,
-        length: 118,
-	director: "Ridley Scott",
-    },
-    {
-        id: 4,
-        name: "Metropolis",
-        genre: "Sci-Fi",
-        publishing year: 1927,
-        length: 149,
-	director: "Fritz Lang",
-    },	
+  {
+    id: 1,
+    name: "Psycho",
+    genre: "Horror",
+    publishingYear: 1960,
+    length: 109,
+    director: "Alfred Hitchcock",
+  },
+  {
+    id: 2,
+    name: "The Good, the Bad and the Ugly",
+    genre: "Western",
+    publishingYear: 1966,
+    length: 161,
+    director: "Sergio Leone",
+  },
+  {
+    id: 3,
+    name: "Balde Runner",
+    genre: "Sci-Fi",
+    publishingYear: 1982,
+    length: 118,
+    director: "Ridley Scott",
+  },
+  {
+    id: 4,
+    name: "Metropolis",
+    genre: "Sci-Fi",
+    publishingYear: 1927,
+    length: 149,
+    director: "Fritz Lang",
+  },
 ];
 
 export const getClassic = (req, res) => {
-    res.status(200).send(classic);
+  res.status(200).send(classic);
 };
 
 export const findClassic = (req, res) => {
-    let result = classic.filter((classic) => classic.name == req.query.classic);
-    res.status(200).send(result);
+  let result = classic.filter((classic) => classic.name == req.query.classic);
+  res.status(200).send(result);
 };
 
 export const findClassicById = (req, res) => {
-    let result = classic.filter((classic) => classic.id == req.params.id);
-    res.status(200).send(result);
+  let result = classic.filter((classic) => classic.id == req.params.id);
+  res.status(200).send(result);
 };
 
 export const addClassic = (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
-    let classic = req.body;
-    classic.push(classic);
-    res.status(201).send(`Added ${classic.name} to classic collection`);
-  };
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  let classic = req.body;
+  classic.push(classic);
+  res.status(201).send(`Added ${classic.name} to classic collection`);
+};
 
 // attached as second param in a route
 export const newCalssicValidators = [
-    check("name").notEmpty().withMessage("Name field required"),
-    check("genre").notEmpty().withMessage("Genre field required"),
-    check("director").notEmpty().withMessage("Director field required"),
-  ];
-
-
+  check("name").notEmpty().withMessage("Name field required"),
+  check("genre").notEmpty().withMessage("Genre field required"),
+  check("director").notEmpty().withMessage("Director field required"),
+];
