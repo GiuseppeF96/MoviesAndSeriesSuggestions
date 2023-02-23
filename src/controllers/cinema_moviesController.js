59ca404eb263df436ef6421b466bf0e81ac52325
Julia <julia-befus.14@web.de>
2023-02-23 11:25:18 +0100

    updated cinema_movies Controller

--- src/controllers/cinema_moviesController.js
+++ src/controllers/cinema_moviesController.js
@@ -1,64 +1,39 @@
 import { check, validationResult } from "express-validator";
-const cinema_movies = [
-  {
-    id: 1,
-    name: "Ant-Man and the Wasp: Quantumania",
-    genre: "Action",
-    publishing: 2023,
-    length: 125,
-    director: "Peyton Reed",
-  },
-  {
-    id: 2,
-    name: "Avatar: The Way of Water",
-    genre: "Sci-Fi",
-    publishing: 2022,
-    length: 195,
-    director: "James Cameron",
-  },
-  {
-    id: 3,
-    name: "Der Geschmack der kleinen Dinge",
-    genre: "Comedy",
-    publishing: 2023,
-    length: 114,
-    director: "Slony Sow",
-  },
-];
+import { Cinema } from "../models/cinema_movie.js";
 
-export const getCinema_movie = (req, res) => {
+export const getCinema_movie = async (req, res) => {
+  const cinema_movies = await Cinema.find ();
   res.status(200).send(cinema_movies);
 };
 
-export const findCinema_movie = (req, res) => {
-  let result = cinema_movies.filter(
-    (serie) => cinema_movies.name == req.query.name
-  );
+export const findCinema_movieByName = async (req, res) => {
+  let result = await Cinema.find({name: req.query.name});
   res.status(200).send(result);
 };
 
-export const findCinema_movieById = (req, res) => {
-  let result = cinema_movies.filter((movie) => movie.id == req.params.id);
+export const findCinema_movieById = async (req, res) => {
+  let result = await Cinema.findById(req.params.id);
   res.status(200).send(result);
 };
 
-export const addCinema_movie = (req, res) => {
+export const addCinema_movie = async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json({ errors: errors.array() });
   }
-  let cinema_movie = req.body;
-  cinema_movies.push(cinema_movie);
-  res
-    .status(201)
-    .send(`Added ${cinema_movie.title} to cinema movie collection`);
+  const cinema_movie = new Cinema({
+    name: req.body.name,
+    genre: req.body.genre,
+    publishing: req.body.publishing,
+    length: req.body.length,
+    director: req.body.director,
+  })
+    cinema_movie.save(cinema_movie).then((cinema_movie) => res.status(201).send(cinema_movie));
 };
 
 // attached as second param in a route
 export const newCinMovieValidators = [
   check("name").notEmpty().withMessage("Name field required"),
   check("genre").notEmpty().withMessage("Genre field required"),
-  check("publishing").notEmpty().withMessage("Publishing field required"),
-  check("length").notEmpty().withMessage("Length field required"),
   check("director").notEmpty().withMessage("Director field required"),
 ];
