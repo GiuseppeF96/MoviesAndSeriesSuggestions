9df2db790c921614b156a19e24b1d452e4217284
Giuseppe <85261127+GiuseppeF96@users.noreply.github.com>
2023-03-06 15:36:08 +0100

    Frontend now working and displaying all 3 collections

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,9 @@
-import React, { useState } from "react";
-import { fetchRandomSerie } from "./RestClient";
+import React from "react";
+import {
+  fetchRandomSerie,
+  fetchRandomCinemaMovie,
+  fetchRandomClassic,
+} from "./RestClient";
 import "./App.css";
 
 class App extends React.Component {
@@ -8,7 +12,9 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      randomSerie: "",
+      randomSerie: {},
+      randomCinema: {},
+      randomClassic: {},
     };
     this.fetchDisplayData = this.fetchDisplayData.bind(this);
   }
@@ -17,8 +23,14 @@ class App extends React.Component {
   // fetchRandomSerie. as soon as the data is there it is set
   // as a state
   async fetchDisplayData() {
-    let data = await fetchRandomSerie();
-    this.setState({ data });
+    let dataSerie = await fetchRandomSerie();
+    let dataCinema = await fetchRandomCinemaMovie();
+    let dataClassic = await fetchRandomClassic();
+    this.setState({
+      randomSerie: dataSerie,
+      randomCinema: dataCinema,
+      randomClassic: dataClassic,
+    });
   }
 
   // this is displayed on the screen
@@ -27,11 +39,28 @@ class App extends React.Component {
       <div>
         <div id="title">Random Serie and Movie Suggestion </div>
         <button id="fetcher" onClick={this.fetchDisplayData}>
-          Click me
+          Get Random Serie and Movies
         </button>
         <div className="data">
           <h1>Random Serie</h1>
-          <p>{this.state.data}</p>
+          <p> {this.state.randomSerie.name} </p>
+          <p> {this.state.randomSerie.genre} </p>
+          <p> {this.state.randomSerie.seasons} </p>
+          <p> {this.state.randomSerie.episodes} </p>
+        </div>
+        <div className="data">
+          <h1>Random Cinema Movie</h1>
+          <p> {this.state.randomCinema.name} </p>
+          <p> {this.state.randomCinema.genre} </p>
+          <p> {this.state.randomCinema.seasons} </p>
+          <p> {this.state.randomCinema.episodes} </p>
+        </div>
+        <div className="data">
+          <h1>Random Classic Movie</h1>
+          <p> {this.state.randomClassic.name} </p>
+          <p> {this.state.randomClassic.genre} </p>
+          <p> {this.state.randomClassic.seasons} </p>
+          <p> {this.state.randomClassic.episodes} </p>
         </div>
       </div>
     );
